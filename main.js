828c81837ce76e57e7452fd43f9c585937fe45ec
saverioannpy <saverio.annunziata@outlook.com>
2023-11-17 02:52:08 +0100

    feat: implementazione click su caselle

--- main.js
+++ main.js
@@ -3,23 +3,47 @@
 
 /** Funzioni **/
 
+
+function clickOnCell(allCells , positionCellClicked, positionsCellBomb){
+
+    console.log("Casella cliccata " + positionCellClicked + 1); //la queryselecton di linea 85 crea un nodo di elementi con indici a partire da zero, quindi alla istruzione facciamo visualizzare in console i + 1;
+    if(positionsCellBomb.includes(positionCellClicked)){
+        for(let i = 1; i <= allCells.length; i++){ //se ll'utente ha cliccato su una bomba con questo for viene fatto scorrere l'intero array e viene aggiunta una classe alle celli presenti nell'array contententi le posizioni delle bombe;
+            if(positionsCellBomb.includes(i)){ //se l'array contenente le posizioni delle bombe contiene la posizione della griglia a cui punta il nostro for ->
+            allCells[i].classList.add("actived-bomb-cell");// <- allora la nostra cella viene colorata di rosso.
+            }   
+        }
+        console.log("Hai cliccato su una bomba!");
+     }else{
+        allCells[positionCellClicked].classList.add("actived-safe-cell");
+        console.log("Hai cliccato su una casella safe, GG!");
+     }
+}
+
 function createBombs(bombsNumberMax){ //funzione creazione array di bombe
     let x;
     const arrayBombs = []; 
     for( let i = 1 ; i <= 16 ; i++){
         do{
-            x = Math.floor(Math.random() * bombsNumberMax) + 1
+            x = Math.floor(Math.random() * bombsNumberMax) + 1 //genero un numero casuale grazie al parametro importato
             console.log("Numero bomba generato: " + x);
-            if(arrayBombs.includes(x)){
+            if(arrayBombs.includes(x)){  //controllo che il numero generato non sia già presente nell'array per evitare coppie di numeri
                 console.log("Attenzione: Numero scartato perchè già presente nell'array.");
             }
         }while(arrayBombs.includes(x))
-        arrayBombs[i] = x ;
+        arrayBombs[i] = x ; //se il numero generato non è già presente nell'array verrà scritto. 
         console.log("Numero bomba inserito nell'array: " + arrayBombs[i]);
     }
-    console.log("Bomber generate pronte per fare fuoco!");
+    console.log("Bomber generate, pronte per fare fuoco!"); //l'array di bombe è pronto
     console.log(arrayBombs);
     console.log(arrayBombs.length);
+    const selectedAllCell = document.querySelectorAll(".cell"); //seleziono tutte le celle create
+    console.log(selectedAllCell);
+    for(let i = 0; i < selectedAllCell.length; i++) {
+        selectedAllCell[i].addEventListener('click', function(){ //applico a tutte le caselle un evento in attesa di un click
+            clickOnCell(selectedAllCell, i , arrayBombs); 
+        });
+      }
 }
 
 function createTable(){ //funzione per la creazione della griglia di gioco
@@ -75,5 +99,8 @@ function createTable(){ //funzione per la creazione della griglia di gioco
 
 const playButton = document.getElementById("play-button"); //seleziono il tasto play
 playButton.addEventListener("click", function(){ //evento sul tasto play
-    createTable();
+    createTable(); //la tabella viene creata dall'apposita funzione
 })
+
+
+
